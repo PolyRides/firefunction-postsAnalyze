db8f003c149770ea76616de5cd59e37a3c3b90fd
Wenmin He <wenmin.he518@gmail.com>
2018-05-01 12:29:08 -0700

    Add in the firebase related stuff

---
+++ functions/index.js
@@ -0,0 +1,58 @@
+const functions = require('firebase-functions');
+// const spacyNLP = require("spacy-nlp");
+var admin = require('firebase-admin');
+var nlp = require('compromise')
+var speak = require("speakeasy-nlp");
+// const nlp = spacyNLP.nlp;
+admin.initializeApp(functions.config().firebase);
+
+var database = admin.database();
+
+const rideOffermockData = {
+origin: "San Luis Obispo, CA", 
+originLat: 35.30199,
+originLon: -120.66381, 
+Destination: "San Jose, CA", // (City, State)
+destinationLat: 37.3382,
+destinationLon: -121.8863,
+departureDate: "04/20/2018 14:00",
+seats: 3,
+cost: 20,
+description: "This is a ride"
+}
+
+
+exports.ProcessPost = functions.database.ref('/posts/')
+    .onCreate((snapshot, context) => {
+      // Grab the current value of what was written to the Realtime Database.
+      const original = snapshot.val();
+      console.log('Uppercasing', context.params.pushId, original);
+      const uppercase = original.toUpperCase();
+      // You must return a Promise when performing asynchronous tasks inside a Functions such as
+      // writing to the Firebase Realtime Database.
+      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+      // var doc = original.replace('$', "")
+      // var result = nlp(doc)
+      // nlp.parse(original).then(output => {
+      //   console.log(output);
+      //   return snapshot.ref.parent.child('RideOffer/').push({output})
+      // })
+      // .catch(error => {
+      //   console.log(error);
+      // })
+      // return "DONE";
+      var result = speak.classify(original)
+      console.log(result, " <<< is the result")
+      return snapshot.ref.parent.child('RideOffer/').push({result})
+      // 
+    });
+
+exports.CreatePost = functions.https.onRequest((request, response) => {
+  // var rideOfferRef = database.ref();
+
+  // Put mock data to the firebase realtime database
+  // riderO
+  database.ref("RideOffer/").push({rideOffermockData})
+  response.send("DONE");
+
+})
